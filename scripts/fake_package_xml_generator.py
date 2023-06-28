3dec21a241ac2eb5b09096ef4f8f64b037cc58d7
Mirko Ferrati <mirko.ferrati@gmail.com>
2023-06-28 15:00:43 +0200

    cleanup and docs

--- scripts/fake_package_xml_generator.py
+++ scripts/fake_package_xml_generator.py
@@ -2,7 +2,20 @@ import os
 import re
 import sys
 
+"""
+Manual script to pre-fill all the needed package.xml files for the snap generation.
+The generated xml files are not production ready and have to be edited manually to fix quirks and corner cases
+Use this script when a new update to the gazebo upstream source requires to bump the xmls in this repository:
+- clone all the ignition packages into a folder
+- run this script using the top folder as input
+- manually edit the output and move it inside the gz_packages_XML folder
+"""
+
 def find_deps_names(folder_path):
+    """
+    Find all dependencies in the CMakeLists.txt inside folder_path.
+    Warn: This is tuned for ignition cmake macros
+    """
     deps_names = []
 
     cmake_file = os.path.join(folder_path, "CMakeLists.txt")
@@ -13,19 +26,21 @@ def find_deps_names(folder_path):
     with open(cmake_file, 'r') as file:
       for line in file:
           if "find_package(ignition-cmake2" in line:  # not using ign_find_package for this
-              print("ignition-cmake2")
               deps_names.append("ignition-cmake2")
           matches = re.search(pattern, line)
           if matches:
               first_word = matches.group(1)
               if "IgnProtobuf" in first_word:
                   continue  # GZ wraps protobuf with their own cmake macro, ignore
-              print(first_word)
               deps_names.append(first_word)
     return deps_names
 
 
 def find_version(folder_path):
+    """Extracts the version number from the CMakeLists.txt inside folder_path
+    Note: this is tuned for ignition cmake project style
+    Warn: This will silently return 0.0.1 if the CMakeLists does not have a version declared
+    """
     cmake_file = os.path.join(folder_path, "CMakeLists.txt")
     if not os.path.isfile(cmake_file):
         raise Exception("CMakeLists.txt not found")
@@ -42,6 +57,11 @@ def find_version(folder_path):
     return "0.0.1"
 
 def find_name(folder_path):
+    """
+    Extracts the project name from the CMakeLists.txt inside folder_path
+    Note: this is tuned for ignition cmake project style
+    Warn: This will silently return NOT_FOUND if the CMakeLists does not have a version declared
+    """
     cmake_file = os.path.join(folder_path, "CMakeLists.txt")
     if not os.path.isfile(cmake_file):
         raise Exception("CMakeLists.txt not found")
@@ -58,6 +78,16 @@ def find_name(folder_path):
     return "NOT_FOUND"
 
 def generate_package_xml(folder_path, package, version, deps_names, xml_name="package.xml"):
+    """
+    Generates a package.xml file based on the information provided in the args
+
+    Args:
+        folder_path (_type_): the folder where package.xml will be created
+        package (_type_): the name of the package 
+        version (_type_): the version of the package
+        deps_names (_type_): a list of dependencies of the package
+        xml_name (str, optional): Name of the output xml file. Defaults to "package.xml".
+    """
     package_xml = os.path.join(folder_path, xml_name)
     with open(package_xml, "w") as f:
       f.write('<?xml version="1.0"?>\n')
@@ -79,17 +109,19 @@ def subdirs(path):
             yield entry.name
 
 def process_folders(top_folder):
-  for folder in os.listdir(top_folder):
+    """
+    Generate a custom package.xml in the top_folder for each subfolder that has a CMakeLists.txt inside
+    Each custom package.xml will have the name of the subfolder used for finding information
+    """
+    for folder in os.listdir(top_folder):
       folder_path = os.path.join(top_folder, folder)
       print("Processing folder "+folder_path)
       deps_names = find_deps_names(folder_path)
       if deps_names:
           version = find_version(folder_path)
           name = find_name(folder_path)
-          #generate_package_xml(folder_path, name, version, deps_names)
           generate_package_xml(top_folder, name, version, deps_names, folder+".xml")
 
-# Read the top-level folder from command-line argument
 if len(sys.argv) < 2:
     print("Please provide the top-level folder as a command-line argument.")
     sys.exit(1)
--- scripts/override_pull.sh
+++ scripts/override_pull.sh
@@ -19,19 +19,11 @@
 
       sed -i "s|\${CMAKE_INSTALL_PREFIX}|/snap/$SNAPCRAFT_PROJECT_NAME/current/opt/ros/snap|" gz-sim/include/ignition/gazebo/config.hh.in
       sed -i "s|\${CMAKE_INSTALL_PREFIX}|/snap/$SNAPCRAFT_PROJECT_NAME/current/opt/ros/snap|" gz-gui/include/gz/gui/config.hh.in
-     # sed -i "s|\"IGN_RENDERING_PLUGIN_PATH\"|\"/snap/$SNAPCRAFT_PROJECT_NAME/current/opt/ros/snap/lib\"|" gz-rendering/src/RenderEngineManager.cc
-     # sed -i "s|OGRE2_RESOURCE_PATH|\"/snap/$SNAPCRAFT_PROJECT_NAME/current/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/OGRE-2.1/OGRE\"|" gz-rendering/ogre2/src/Ogre2RenderEngine.cc
-     # sed -i "s|OGRE_RESOURCE_PATH|\"/snap/$SNAPCRAFT_PROJECT_NAME/current/usr/lib/$SNAPCRAFT_ARCH_TRIPLET/OGRE-1.9.0\"|" gz-rendering/ogre/src/OgreRenderEngine.cc
-     # sed -i "s|@IGN_SENSORS_PLUGIN_PATH@|/snap/$SNAPCRAFT_PROJECT_NAME/current/opt/ros/snap/lib|" gz-sensors/include/gz/sensors/config.hh.in
-     # sed -i 's|\${IGNITION_RENDERING_ENGINE_INSTALL_DIR}|/snap/$SNAPCRAFT_PROJECT_NAME/current/opt/ros/snap/lib/ign-rendering-3/engine-plugins|' gz-rendering/include/gz/rendering/config.hh.in
 
       # artificial ign package.xml are not "installed" so rosdep try to redownload them
       sed -i 's|<depend>ignition-plugin<\/depend>|<build_depend>ignition-plugin1<\/build_depend>|' ign_ros2_control/ign_ros2_control/package.xml
       sed -i '/ignition-.*<\/depend/s/depend/build_depend/g' ign_ros2_control/ign_ros2_control/package.xml
       find ros_gz -name package.xml -exec sed -i '/ignition-.*<\/depend/s/depend/build_depend/g' {} \;
-
-      # replace dependencies to ignition- packages to gazebo- packages
-      #find . -name package.xml -exec sed -i '/ignition-.*</s/ignition/gazebo/g' {} \;
 
       sed -i '/ros_ign_gazebo_demos/d' ros_gz/ros_ign/package.xml
       sed -i '/ros_gz_sim_demos/d' ros_gz/ros_gz/package.xml
