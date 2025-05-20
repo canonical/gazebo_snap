689e957366e9cdfc7eb23dcab089789fc8df4e6d
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2025-05-20 18:19:06 +0200

    feat(harmonic): Gazebo now has vendor packages to be built with ROS

--- snap/local/override_pull.sh
+++ snap/local/override_pull.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-      if [ ! -f collection-fortress.yaml ]; then wget https://raw.githubusercontent.com/gazebo-tooling/gazebodistro/master/collection-fortress.yaml; fi
-      vcs import < collection-fortress.yaml
-      vcs import < ros2-ign.yaml
 
-      # delete the directory since we don't want rosdep to pull the deps
-      rm -rf ign_ros2_control/gz_ros2_control_demos
-      rm -rf ign_ros2_control/gz_ros2_control_tests
-      rm -rf ign_ros2_control/ign_ros2_control_demos
-      rm -rf ros_gz/ros_gz_sim_demos
-      rm -rf ros_gz/ros_ign_gazebo_demos
+vcs import < gz-vendors.yaml
+vcs import < ros2-gz.yaml
 
-      # you can use fake_package_xml_generator to prepare a draft for gz_packages_XML based on the output of vcs import
-      # python3 scripts/fake_package_generator.py [folder_where_vcs_imported_gz_repos]
 
-      # place artifical package.xml files in ign directories
-      ls gz_packages_XML | awk -F. '{printf("mv gz_packages_XML/%s.xml %s/package.xml\n", $1, $1)}' | sh
 
       sed -i "s|\${CMAKE_INSTALL_PREFIX}|/snap/$SNAPCRAFT_PROJECT_NAME/current/opt/ros/snap|" gz-gui/include/gz/gui/config.hh.in
       sed -i "s|\${CMAKE_INSTALL_PREFIX}|/snap/$SNAPCRAFT_PROJECT_NAME/current/opt/ros/snap|" gz-sim/include/gz/sim/config.hh.in
