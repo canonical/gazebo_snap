2bbca01b1fa72f14d4138ed039322f8d8a41031d
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2025-05-20 17:11:16 +0200

    wip

--- snap/local/override_pull.sh
+++ snap/local/override_pull.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-#if [ ! -f collection-harmonic.yaml ]; then wget https://raw.githubusercontent.com/gazebo-tooling/gazebodistro/master/collection-harmonic.yaml; fi
-
-#vcs import < collection-harmonic.yaml
 vcs import < gz-vendors.yaml
 vcs import < ros2-gz.yaml
 
-#cp patches/gz_ogre_next_vendor/fix-math-namespace.patch gz_ogre_next_vendor/patches/
-#sed -i "s|PATCHES|PATCHES\n    patches/fix-math-namespace.patch|" gz_ogre_next_vendor/CMakeLists.txt
-
+# patches to remove CMAKE_INSTALL_PATH inserted in the libraries
 cp patches/gz_rendering_vendor/remove_cmake_gz_rendering_plugin_path.patch gz_libs/gz_rendering_vendor/patches/
-
-# cp patches/test.patch gz_libs/gz_rendering_vendor/patches/
-# cp patches/test1.patch gz_libs/gz_rendering_vendor/patches/
-cp patches/test2.patch gz_libs/gz_rendering_vendor/patches/
-
+cp patches/gz_rendering_vendor/remove_install_path_search.patch gz_libs/gz_rendering_vendor/patches/
 
 # delete the directory since we don't want rosdep to pull the deps
 rm -rf gz_ros2_control/gz_ros2_control_demos
--- snap/local/path_modifier.sh
+++ snap/local/path_modifier.sh
@@ -5,14 +5,8 @@ original_args=("$@")
 
 if [ "$SNAP_ARCH" = "amd64" ]; then
   ARCH_TRIPLET="x86_64-linux-gnu"
-elif [ "$SNAP_ARCH" = "armhf" ]; then
-  ARCH_TRIPLET="arm-linux-gnueabihf"
 elif [ "$SNAP_ARCH" = "arm64" ]; then
   ARCH_TRIPLET="aarch64-linux-gnu"
-elif [ "$SNAP_ARCH" = "ppc64el" ]; then
-  ARCH_TRIPLET="powerpc64le-linux-gnu"
-else
-  ARCH_TRIPLET="$SNAP_ARCH-linux-gnu"
 fi
 
 SNAP_LIB_PATH="$SNAP/usr/local/lib/:$SNAP/usr/local/lib/$ARCH_TRIPLET/:$SNAP/usr/lib/:$SNAP/usr/lib/$ARCH_TRIPLET/:$SNAP/usr/lib/$ARCH_TRIPLET/blas:$SNAP/usr/lib/$ARCH_TRIPLET/lapack"
