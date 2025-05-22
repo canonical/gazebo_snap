645d7f4a5493e52cdecbb0176cf5fb5da8b0e08e
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2025-05-22 15:38:08 +0200

    fix: pr comments

--- snap/local/path_modifier.sh
+++ snap/local/path_modifier.sh
@@ -12,7 +12,5 @@ fi
 SNAP_LIB_PATH="$SNAP/usr/local/lib/:$SNAP/usr/local/lib/$ARCH_TRIPLET/:$SNAP/usr/lib/:$SNAP/usr/lib/$ARCH_TRIPLET/:$SNAP/usr/lib/$ARCH_TRIPLET/blas:$SNAP/usr/lib/$ARCH_TRIPLET/lapack"
 RUBY_LIB_PATH="$SNAP/usr/local/lib/site_ruby/3.2.0:$SNAP/usr/local/lib/$ARCH_TRIPLET/site_ruby:$SNAP/usr/local/lib/site_ruby:$SNAP/usr/lib/ruby/vendor_ruby/3.2.0:$SNAP/usr/lib/$ARCH_TRIPLET/ruby/vendor_ruby/3.2.0:$SNAP/usr/lib/ruby/vendor_ruby:$SNAP/usr/lib/ruby/3.2.0:$SNAP/usr/lib/$ARCH_TRIPLET/ruby/3.2.0"
 export LD_LIBRARY_PATH=$SNAP_LIB_PATH:$RUBY_LIB_PATH:${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=$SNAP_LIB_PATH:$RUBY_LIB_PATH:${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-
 
 exec "${original_args[@]}"
